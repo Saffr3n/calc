e39a14fbd31425c8905184e93963aedb750e0822
Saffr3n <saffr3n@gmail.com>
2022-09-19 22:50:28 +0300

    Edit script.js
    1. added operate() function
    2. added +/- button behaviour
    3. added dot button behaviour
    4. added = button behaviour
    5. added behaviour for more than 1 operator in input
    
    TODO:
    1. add behaviour for = button when it's pressed without entering 2nd operand
    2. add M button behaviour
    3. fix division by zero

--- script.js
+++ script.js
@@ -3,44 +3,86 @@ const display = document.querySelector('#disp');
 
 let input = ['0'];
 
-buttons.forEach(btn =>{
+buttons.forEach(btn =>{ // TODO: 1. handle Infinity result; 2. memory button behaviour
     btn.addEventListener('click', e => {
-        switch (e.target.value) {
-            case 'C':
-                input = ['0'];
-                break;
-            case 'M':
-                break;
-            case 'B':
-                input.pop();
-                if (input.length === 0) input = ['0'];
-                break;
-            case 'S':
-                break;
-            case '=':
-                break;
-            case '.':
-                break;
-            default:
-                if ((e.target.className.includes('num') && input.length === 1 && input[0] === '0') ||
-                    (e.target.className.includes('op') && /[\+\-\*\/]/.test(input[input.length - 1])))
-                    input.pop();
-                input.push(e.target.value);
+        if (e.target.value === 'C')
+            input = ['0'];
+
+        if (e.target.value === 'B') {
+            input.pop();
+            if (input.length === 0) input = ['0'];
         }
 
+        if (e.target.className.includes('num') || e.target.className.includes('op')) {
+            if (e.target.className.includes('num') && input.length === 1 && input[0].includes('0'))
+                input[0] = input[0].replace('0', e.target.value);
+            else if (e.target.className.includes('op') && /[\+\-\*\/]$/.test(input[input.length - 1])) {
+                input.pop();
+                input.push(e.target.value);
+            }
+            else input.push(e.target.value);
+        }
+
+        let operatorIndex = input.findIndex(char => /[\+\-\*\/]$/.test(char));
         let operatorsAmount = input.reduce((n, char) => {
-            if (/[\+\-\*\/]/.test(char)) n++;
+            if (/[\+\-\*\/]$/.test(char)) n++;
             return n;
         }, 0);
 
-        if (operatorsAmount > 1) {
-            // TODO: Calculate expression; input = [result, last_used_operator]
-            input = ['0']; // reset for now
+        if (e.target.value === 'S' && input[operatorIndex + 1]) {
+            if (input[operatorIndex + 1].length === 1)
+                input[operatorIndex + 1] = '-' + input[operatorIndex + 1];
+            else input[operatorIndex + 1] = input[operatorIndex + 1].replace('-', '');
         }
 
-        let operatorIndex = input.findIndex(char => /[\+\-\*\/]/.test(char));
-        if (operatorIndex !== -1 && operatorIndex !== input.length - 1)
+        if (e.target.value === '.' && !input.slice(operatorIndex + 1).includes('.')) {
+            if (operatorIndex === input.length - 1) input.push('0');
+            input.push(e.target.value);
+        }
+
+        if (e.target.value === '=') { // TODO: make behaviour for when = pressed without second operand
+            input = [...operate(operatorIndex)];
+            operatorIndex = -1;
+            operatorsAmount = 0;
+        }
+
+        if (operatorsAmount > 1) {
+            const operator = input[input.length - 1];
+            input.pop();
+            input = [...operate(operatorIndex), operator];
+            operatorIndex = input.findIndex(char => /[\+\-\*\/]$/.test(char));
+            operatorsAmount = 1;
+        }
+
+        if (operatorIndex !== input.length - 1)
             display.textContent = input.slice(operatorIndex + 1).join('');
         else display.textContent = input.join('');
     });
 });
+
+function operate (operatorIndex) {
+    const x = parseFloat(input.slice(0, operatorIndex).join(''));
+    const y = parseFloat(input.slice(operatorIndex + 1).join(''));
+    let result;
+
+    switch (input[operatorIndex]) {
+        case '+':
+            result = `${x + y}`.split('');
+            break;
+        case '-':
+            result = `${x - y}`.split('');
+            break
+        case '*':
+            result = `${x * y}`.split('');
+            break
+        case '/':
+            result = `${x / y}`.split('');
+    }
+
+    if (result[0] === '-') {
+        result[1] = '-' + result[1];
+        result.shift();
+    }
+
+    return result;
+}
